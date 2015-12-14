db20232cf7b25f53eceb72cbfa9daec0f0dd5ce4
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2015-12-14 17:09:47 -0200

    link das telas e criacao da TableViewController

--- ScreensApp/ScreensApp/ViewController.swift
+++ ScreensApp/ScreensApp/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tblListaEventos: UITableView!
+    @IBOutlet weak var lblDataHoje: UILabel!
+    var eventos : Array<Evento>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +26,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return eventos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! TableViewCell
+        
+        cell.lblNomeEvento.text = eventos[indexPath.row].nome_evento
+        cell.lblCategoria.text = eventos[indexPath.row].categoria
+        cell.
+        return cell
+    }
+    
+
 
 
 }
