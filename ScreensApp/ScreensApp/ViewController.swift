9699079f0354b2965ae09517cc21c13f6834eb1c
Cristiano Oliveira <cris_2008_@hotmail.com>
2015-12-14 17:39:21 -0200

    Revert "link das telas e criacao da TableViewController"
    This reverts commit db20232cf7b25f53eceb72cbfa9daec0f0dd5ce4.

--- ScreensApp/ScreensApp/ViewController.swift
+++ ScreensApp/ScreensApp/ViewController.swift
@@ -8,17 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-    
-    
-    @IBOutlet weak var tblListaEventos: UITableView!
-    @IBOutlet weak var lblDataHoje: UILabel!
-    var eventos : Array<Evento>()
-    
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,21 +19,6 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return eventos.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! TableViewCell
-        
-        cell.lblNomeEvento.text = eventos[indexPath.row].nome_evento
-        cell.lblCategoria.text = eventos[indexPath.row].categoria
-        cell.
-        return cell
-    }
-    
-
 
 
 }
